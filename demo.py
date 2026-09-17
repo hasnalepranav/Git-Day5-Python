d1c30887bcfe3a3a5e7cd4d4cd72d1df2ef35a86
hasnalepranav <167642525+hasnalepranav@users.noreply.github.com>
2026-09-17 19:57:26 +0530

    New File Added

--- demo.py
+++ demo.py
@@ -1,13 +1,3 @@
-# def greet():
-#     print("Hello")
-# greet()
-
-# def greet(name="student"):
-#     print("Hello",name)
-# greet()
-# greet("Pranav")
-
-# def square(n):
-#     return n*n 
-# print(square(5))
-
+def greet():
+    print("Hello")
+greet()
